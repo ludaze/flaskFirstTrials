8c3d1b4d3093ac7546d482c7b580189e6daaab53
luhana daniel <se.luhana.daniel@gmail.com>
2021-04-29 12:14:11 +0300

    init page elements edited

--- __init__.py
+++ __init__.py
@@ -1,15 +1,30 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+
+from flask import Flask, session,render_template,request,redirect,url_for,jsonify
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import hashlib
+import os
+import requests
+
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt=Bcrypt(app)
-login_manager= LoginManager(app)
-login_manager.login_view='login'
-login_manager.login_message_category = 'info'
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
-from flaskblog import routes
+# Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
